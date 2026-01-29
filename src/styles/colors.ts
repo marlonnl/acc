877ca0b91c2b5a187c632a8c681026ee06e7a3f7
Marlonn Locatelli <marlonn@gmail.com>
2026-01-28 23:15:26 -0300

    💄 setting colors (initial)

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,13 @@
 export const color = {
 	// backgrounds
-	bg: '#D9D9D9'
+	bg: '#D9D9D9',
+	headerBg: '#5A5A5A',
+	infoBarBg: '#BDBDBD',
+	cellContainerBg: '#B2B2B2',
+
+	// text colors
+	h1Color: '#EBEBEB',
+	cellNameColor: '#EBEBEB',
+	keyColor: '#868686',
+	cellCountColor: '#595959'
 }

376d6656de2133756c3df43cdf1e77a31e012300
Marlonn Locatelli <marlonn@gmail.com>
2026-02-05 18:21:58 -0300

    ✨ added total count from context

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react'
 import CellList from '../../containers/CellList'
 import { CellGrid, HomeContainer } from './styles'
+import { CellContext } from '../../context/context'
 
 const Home = () => {
+	const cells = useContext(CellContext)
+
+	const total = cells!.reduce((acc, { count }) => acc + count, 0)
+
 	return (
 		<div className="container">
 			<HomeContainer>
 				<h3>
-					Total: <span className="totalCell">0</span>
+					Total: <span className="totalCell">{total}</span>
 				</h3>
 				<CellGrid>
 					<CellList />
-					<CellList />
+					{/* <CellList /> */}
 				</CellGrid>
 			</HomeContainer>
 		</div>

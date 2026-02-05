17dc8416d4fb339b2c174a8bd6251c58a7a38aae
Marlonn Locatelli <marlonn@gmail.com>
2026-02-05 18:23:21 -0300

    ✨ added context to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,32 @@ import InfoBar from './containers/InfoBar'
 
 import { GlobalStyle } from './styles'
 import { BrowserRouter } from 'react-router-dom'
-import { CellContext } from './context/context'
-import { MATURE_CELL_LIST } from './utils'
+import { CellContext, CellDispatchContext } from './context/context'
+import { MATURE_CELLS } from './utils/cellGroup'
+import { useReducer } from 'react'
+import { reducer } from './context/reducer'
 
 function App() {
+	const [cells, dispatch] = useReducer(reducer, MATURE_CELLS)
+
 	return (
 		<>
-			<GlobalStyle />
-			<Header />
+			<CellContext.Provider value={cells}>
+				<CellDispatchContext.Provider value={dispatch}>
+					<GlobalStyle />
+					<Header />
 
-			<BrowserRouter>
-				<InfoBar
-					display={false}
-					message={
-						'nova versão do contador lançada em 31/01/2026. Incluído lista de células imaturas. Para ver mais clique aqui.'
-					}
-				/>
-				<CellContext.Provider value={MATURE_CELL_LIST}>
-					<Rotas />
-				</CellContext.Provider>
-			</BrowserRouter>
+					<BrowserRouter>
+						<InfoBar
+							display={false}
+							message={
+								'nova versão do contador lançada em 31/01/2026. Incluído lista de células imaturas. Para ver mais clique aqui.'
+							}
+						/>
+						<Rotas />
+					</BrowserRouter>
+				</CellDispatchContext.Provider>
+			</CellContext.Provider>
 		</>
 	)
 }
